68b1e42b540a9aabacffe32fce86ef48972f0804
Devat Karetha <devat.karetha@viitor.cloud>
2021-02-25 19:12:14 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000/'
+
+store.dispatch('auth/me').then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
